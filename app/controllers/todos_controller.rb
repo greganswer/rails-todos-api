ab9185e0cdea5f26d2ea57c5e604c71c39218432
Greg Answer <greganswer@gmail.com>
2017-11-22 14:15:37 -0500

    Todos request spec is passing

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,42 @@
+class TodosController < ApplicationController
+end
+class TodosController < ApplicationController
+  before_action :set_todo, only: %i(show update destroy)
+
+  def index
+    json_response(Todo.all)
+  end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
+  end
+
+  def show
+    json_response(@todo)
+  end
+
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  #
+  # PRIVATE
+  #
+
+  private
+
+  def todo_params
+    params.permit(:title, :created_by)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+end
