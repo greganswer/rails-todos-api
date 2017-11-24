bf931c0484f3f4347cacf45194b9a86a0b6d5df6
Greg Answer <greganswer@gmail.com>
2017-11-23 20:05:47 -0500

    Accept diferent API versions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  resources :todos do
-    resources :items
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+
+  # Non-default versions have to be defined above the default version
+
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
- 
